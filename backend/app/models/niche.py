96907a3d376d7666fdd5996a18e32f8945c7ae6d
kaiomenes15 <kakacnmendes@gmail.com>
2026-05-25 14:08:36 -0300

    Add niche model schema

--- backend/app/models/niche.py
+++ backend/app/models/niche.py
@@ -1,15 +1,15 @@
 from sqlalchemy import String
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
-
-class Base(DeclarativeBase):
-    pass
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from app.database import Base
+from app.models.user_niches import user_niches
 
 class Niche(Base):
     __tablename__ = "niches"
 
-    # columns
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
     ml_category_id: Mapped[int]
+
+    users: Mapped[list["User"]] = relationship(
+        "User", secondary=user_niches, back_populates="niches"
+    )
