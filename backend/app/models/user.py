9b8087a919d6e74397bfb14a3f4bf8fe3d885ab3
kaiomenes15 <kakacnmendes@gmail.com>
2026-05-25 14:08:56 -0300

    Add user model schema

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,21 +1,21 @@
-from sqlalchemy import String
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
 from datetime import datetime
-
-class Base(DeclarativeBase):
-    pass
+from sqlalchemy import String, DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from app.database import Base
+from app.models.user_niches import user_niches
 
 class User(Base):
     __tablename__ = "users"
 
-    # columns
     id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    username: Mapped[str] = mapped_column(String(255), unique=False, nullable=False)
     hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
     created_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True),
-        server_default=func.now()  # Sets the time on the DB server
+        DateTime(timezone=True), server_default=func.now()
+    )
+
+    # secondary= tells SQLAlchemy to use user_niches as the join table
+    niches: Mapped[list["Niche"]] = relationship(
+        "Niche", secondary=user_niches, back_populates="users"
     )
